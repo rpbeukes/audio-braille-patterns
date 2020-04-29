534135c713603acab779e2af73d177b9f8b8a561
Ruan Beukes <ruan.coder@gmail.com>
2020-04-29 22:30:29 +0800

    add more patterns

--- frontend/src/app/layout/braillePatterns/braille-patterns.component.ts
+++ frontend/src/app/layout/braillePatterns/braille-patterns.component.ts
@@ -1,22 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
+import { BraillePatternLines, BraillePatternLine } from './braillePattterns';
 
-export interface BraillePatternLine {
-    name: string;
-    position?: number;
-    pictureUrl?: string;
-    audioUrl: string;
-}
-
-const DATA: BraillePatternLine[] = [
-    { position: 1, name: 'Hydrogen', pictureUrl: 'https://www.pathstoliteracy.org/sites/pathstoliteracy.perkinsdev1.org/files/uploaded-images/braille_tow_truck.png', audioUrl: 'https://www.youtube.com' },
-    { position: 1, name: 'Helium', pictureUrl: 'https://www.pathstoliteracy.org/sites/pathstoliteracy.perkinsdev1.org/files/uploaded-images/braille_tow_truck.png', audioUrl: 'https://www.youtube.com' },
-    { position: 1, name: 'Lithium', audioUrl: 'https://www.youtube.com' },
-    { position: 1, name: 'Beryllium' , pictureUrl: 'https://www.pathstoliteracy.org/sites/pathstoliteracy.perkinsdev1.org/files/uploaded-images/braille_tow_truck.png', audioUrl: 'https://www.youtube.com' },
-    { position: 1, name: 'Boron' , pictureUrl: 'https://www.pathstoliteracy.org/sites/pathstoliteracy.perkinsdev1.org/files/uploaded-images/braille_tow_truck.png', audioUrl: 'https://www.youtube.com' },
-    { position: 1, name: 'Carbon', pictureUrl: 'https://www.pathstoliteracy.org/sites/pathstoliteracy.perkinsdev1.org/files/uploaded-images/braille_tow_truck.png', audioUrl: 'https://www.youtube.com' },
-    { position: 1, name: 'Nitrogen' , audioUrl: 'https://www.youtube.com' },
-];
+//type BraillePatternColumnNames = Pick<BraillePatternLine, 'position' | 'name' | 'pictureUrl' | 'audioUrl';
 
 @Component({
     selector: 'app-dashboard',
@@ -24,8 +10,11 @@ const DATA: BraillePatternLine[] = [
     styleUrls: ['./braille-patterns.component.scss']
 })
 export class BraillePatternsComponent implements OnInit {
-    displayedColumns = ['position', 'name', 'pictureUrl', 'audioUrl'];
-    dataSource = new MatTableDataSource(DATA);
+    // private fff: keyof BraillePatternLine = 'p1osition';
+    // columnNames = fff
+    displayedColumns =  ['position', 'name', 'pictureUrl', 'audioUrl'];
+
+    dataSource = new MatTableDataSource(BraillePatternLines);
 
     applyFilter(filterValue: string) {
         filterValue = filterValue.trim(); // Remove whitespace
---
+++ frontend/src/app/layout/braillePatterns/braillePattterns.ts
@@ -0,0 +1,45 @@
+export interface BraillePatternLine {
+    name: string;
+    position?: number;
+    pictureUrl?: string;
+    audioUrl: string;
+    blogContentUrl?: string;
+}
+
+export const BraillePatternLines: BraillePatternLine[] = [
+    {
+        position: 1,
+        name: 'Tow truck',
+        blogContentUrl: 'https://www.pathstoliteracy.org/strategies/tow-truck-braille-design',        
+        pictureUrl: 'https://www.pathstoliteracy.org/sites/pathstoliteracy.perkinsdev1.org/files/uploaded-images/braille_tow_truck.png', 
+        audioUrl: 'https://youtu.be/qRyK6Dqu6-c',
+    },
+    {
+        position: 1,
+        name: `Valentine's heart`,
+        blogContentUrl: 'https://www.pathstoliteracy.org/strategies/braille-designs-valentines-day',
+        pictureUrl: 'https://www.pathstoliteracy.org/sites/pathstoliteracy.perkinsdev1.org/files/ValentineHeart.png',
+        audioUrl: 'https://www.youtube.com',
+    },
+    {
+        position: 1,
+        name: `Santa Claus`,
+        blogContentUrl: 'https://www.pathstoliteracy.org/strategies/braille-designs-christmas',
+        pictureUrl: 'https://www.pathstoliteracy.org/sites/pathstoliteracy.perkinsdev1.org/files/braille_santa.png',
+        audioUrl: 'https://www.youtube.com',
+    },
+    {
+        position: 1,
+        name: `Hello Kitty with Santa hat`,
+        blogContentUrl: 'https://www.pathstoliteracy.org/strategies/braille-design-hello-kitty',
+        pictureUrl: 'https://www.pathstoliteracy.org/sites/pathstoliteracy.perkinsdev1.org/files/uploaded-images/hello_kitty_braille_design.png',
+        audioUrl: 'https://www.youtube.com',
+    },
+    {
+        position: 1,
+        name: `Flower in pot`,
+        blogContentUrl: 'https://www.pathstoliteracy.org/strategies/braille-designs-spring',
+        pictureUrl: 'https://www.pathstoliteracy.org/sites/pathstoliteracy.perkinsdev1.org/files/Flowerpot.png',
+        audioUrl: 'https://www.youtube.com',
+    },
+];

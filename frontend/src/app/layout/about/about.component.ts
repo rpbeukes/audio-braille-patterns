4c19fa21104d25cfb6bfe3adb214297d39cb290d
Ruan Beukes <ruan.coder@gmail.com>
2020-05-03 20:16:45 +0800

    lower case About => about

--- frontend/src/app/layout/about/about.component.ts
+++ frontend/src/app/layout/about/about.component.ts
@@ -2,8 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-about',
-  templateUrl: './About.component.html',
-  styleUrls: ['./About.component.scss']
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.scss']
 })
 export class AboutComponent implements OnInit {
 

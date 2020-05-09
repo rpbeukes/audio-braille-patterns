1b0d5ba8881e4835192d32e51fa4bc0f1957b918
Ruan Beukes <ruan.coder@gmail.com>
2020-05-09 16:21:00 +0800

    add Google Analytics -update angular -CI build will replace comment in index.html with meta data required by Google Analytics

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,13 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Router, NavigationEnd } from "@angular/router";
+
+declare let gtag: Function;
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.scss']
+    selector: "app-root",
+    templateUrl: "./app.component.html",
+    styleUrls: ["./app.component.scss"],
 })
 export class AppComponent implements OnInit {
-    constructor() {  }
+    constructor(public router: Router) {}
 
     ngOnInit() {
+        this.router.events.subscribe((event) => {
+            if (event instanceof NavigationEnd) {
+                gtag("config", "UA-123740081-4", {
+                    page_path: event.urlAfterRedirects,
+                });
+            }
+        });
     }
 }
